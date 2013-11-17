f8e8ff23304a20a52a211cee6e3691df55f9707a
Austin Emser <austinemser@Austins-MacBook-Pro.local>
2013-11-17 00:27:04 -0500

    Change menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,7 @@
 class Menu < ActiveRecord::Base
   validates :title, :presence =>true, :uniqueness => true
   
-  has_attached_file :photo#, :styles => { :small => "150x150>" }
+  has_attached_file :photo, :styles => { :small => "150x150>" }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 end
+  
