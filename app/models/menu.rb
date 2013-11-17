eaffe877df96853afa90986a44302d3a730d4a05
Austin Emser <austinemser@Austins-MacBook-Pro.local>
2013-11-17 01:23:13 -0500

    More upload testing

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,9 +1,7 @@
 class Menu < ActiveRecord::Base
   validates :title, :presence =>true, :uniqueness => true
   
-  has_attached_file :photo, :styles => { :small => "150x150>"},
-                            :url => "/assets/menus/:id/:style/:basename.:extension",
-                            :path => ":rails_root/public/assets/menus/:id/:style/:basename.:extension"
+  has_attached_file :photo, :styles => { :small => "150x150>"}
   
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
